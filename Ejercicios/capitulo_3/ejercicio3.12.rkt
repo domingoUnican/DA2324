1de3736432114e272e9cd9a47ead6b7ba4949200
ismaelngmez <126773815+ismaelngmez@users.noreply.github.com>
2023-10-27 18:15:36 +0200

    3.12 y 3.16

---
+++ Ejercicios/capitulo_3/ejercicio3.12.rkt
@@ -0,0 +1,32 @@
+#lang racket
+(define (mappend x y)
+  (if (null? x)
+      y
+      (mcons (mcar x) (mappend (mcdr x) y))))
+
+
+(define (append! x y)
+  (set-mcdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (mcdr x))
+      x
+      (last-pair (mcdr x))))
+
+(define x (mcons 'a (mcons 'b null)))
+(define y (mcons 'c (mcons 'd null)))
+(define z (mappend x y))
+
+z
+(mcons 'a (mcons 'b (mcons 'c (mcons 'd '()))))
+
+(mcdr x) ; b
+
+(define w (append! x y))
+
+w
+(mcons 'a (mcons 'b (mcons 'c (mcons 'd '()))))
+
+(mcdr x) ; b c d 
+
