fac0caeb9c1de8d5dbc7165291d1610f8a02661e
Domingo gomez <domingo.domingogomez@gmail.com>
2023-11-05 12:10:14 +0100

    Revert "Ejercicios 2.2-2.19"

--- Ejercicios/capitulo_1/ej_1.42.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket
-;Ejer 1.42
-
-(define (compose f g)
-  (lamda (n) (f (g n))))
--- Ejercicios/capitulo_2/ej_2.17.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(define (last-pair lista)
-  (if (null? (cdr lista))
-      lista
-      (last-pair (cdr lista))))
-
-(last-pair (list 23 72 149 34))
